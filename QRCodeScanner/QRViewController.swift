04bc16226503e838ecaf97ecc722662b64c4268f
吳登秝 <ji3g4kami@gmail.com>
2018-08-01 15:06:15 +0800

    [fix] LandSacpe can stay same with full screen

--- QRCodeScanner/QRViewController.swift
+++ QRCodeScanner/QRViewController.swift
@@ -12,11 +12,10 @@ import AVFoundation
 class QRViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     
     var video = AVCaptureVideoPreviewLayer()
+    let session = AVCaptureSession()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let session = AVCaptureSession()
         
         // Define capture device
         guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
@@ -42,14 +41,34 @@ class QRViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate
         session.startRunning()
     }
     
+    override func viewDidLayoutSubviews() {
+        self.configureVideoOrientation()
+    }
+    
+    private func configureVideoOrientation() {
+        if let connection = video.connection {
+            let orientation = UIDevice.current.orientation
+            
+            if connection.isVideoOrientationSupported,
+                let videoOrientation = AVCaptureVideoOrientation(rawValue: orientation.rawValue) {
+                video.frame = self.view.bounds
+                video.connection?.videoOrientation = videoOrientation
+            }
+        }
+    }
+    
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
         if metadataObjects.count == 0 { return }
+        session.stopRunning()
         if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject, object.type == .qr {
             let alertController = UIAlertController(title: "QRCode", message: object.stringValue, preferredStyle: .alert)
-            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in
+                self.session.startRunning()
+            }
             let openAction = UIAlertAction(title: "Open", style: .default) { _ in
                 if let urlString = object.stringValue, let url = URL(string: urlString) {
                     UIApplication.shared.open(url, options: [:])
+                    self.session.startRunning()
                 }
             }
             alertController.addAction(cancelAction)
